eac1cfcfae4650dce31f5c1a361881e949ce9d94
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-06-01 11:19:32 +0530

    04:Sign In UI

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import SignUpView from "@/modules/auth/ui/views/sign-up-view";
+
+const SignUp=()=>{
+    return(
+        <SignUpView />
+    )
+
+}
+
+
+export default  SignUp;
---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Card } from '@/components/ui/card'
+import React from 'react'
+
+const SignUpView = () => {
+  return (
+    <Card>SignInView</Card>
+  )
+}
+
+export default SignUpView
